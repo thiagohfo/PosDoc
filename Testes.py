cddecab32d9d6bcccbc67056ffc336a40403d331
Thiago Henrique <83481620+thiagohfo@users.noreply.github.com>
2021-11-09 17:52:26 -0300

    Delete Testes
    Useless file for the project

--- Testes.py
+++
@@ -1,6 +0,0 @@
-#!/opt/anaconda3/envs/PosDoc/bin/python
-from useful_functions import *
-
-data = read_data('Bases/dados-rj-2.csv')
-pd.set_option('display.max_columns', None) # Mostrar todas as colunas
-pd.set_option('display.max_rows', None) # Mostrar apenas 10 linhas
